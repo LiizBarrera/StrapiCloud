de02ca73a8af27792609f879b47e959a203b3c83
LiizBarrera <liize_202@hotmail.com>
2024-11-24 14:15:09 -0600

    Primera subida del proyecto

---
+++ src/api/flor/controllers/flor.ts
@@ -0,0 +1,7 @@
+/**
+ * flor controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::flor.flor');
---
+++ src/api/flor/routes/flor.ts
@@ -0,0 +1,7 @@
+/**
+ * flor router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::flor.flor');
