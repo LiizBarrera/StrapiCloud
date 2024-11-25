7a78b311d0a519ea09e6a0d675dd0de0af378ff7
LiizBarrera <106847162+LiizBarrera@users.noreply.github.com>
2024-11-24 21:22:55 -0600

    Update flor.ts

--- src/api/flor/routes/flor.ts
+++ src/api/flor/routes/flor.ts
@@ -1,7 +1,13 @@
 /**
  * flor router
  */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::flor.flor');
+export default factories.createCoreRouter('api::flor.flor', {
+  config: {
+    delete: {
+      policies: [], // Puedes agregar políticas aquí si es necesario
+      middlewares: [], // Puedes agregar middlewares personalizados aquí si es necesario
+    },
+  },
+});
